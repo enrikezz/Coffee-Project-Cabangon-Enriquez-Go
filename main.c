ea36d1914d475ecbb9190e8d4b6d91603d5c8c8f
enrikezz <andrei_enriquez@dlsu.edu.ph>
2024-03-25 17:16:28 +0800

    total is being given now, final receipt is done

--- main.c
+++ main.c
@@ -2,6 +2,33 @@
 #include <ctype.h>
 #include <string.h>
 
+
+//these variables are declared outside the functions - making them global. Meaning they can be accessed by all functions, without having them be declared in the respective function itself.
+
+//prices of espressos arranged respective to their menu order ex: espresso[0][1] corresponds to 145; or a grande[1] caffe americano[0]
+int espresso_prices[8][3] = {{130,145,160},{140,155,170},{140,155,170},{165,180,195},{165,180,195},{165,180,195},{165,180,195},{120,130,0}};
+char *espressos[50] = {"Caffe Americano","Cappuccino","Caffe Latte","Caffe Mocha","Caramel Macchiato","Flat White","White Chocolate Mocha","Espresso"};
+
+char *frappucino[50] = {"Caramel","Mocha","White Mocha","Coffee Jelly","Java Chip","Dark Mocha","Chocolate Cream","Vanilla Cream","Caramel Cream","Chocolate Chip Cream","Strawberries & Cream","Green Tea Cream"};
+int frappucino_prices[12][3] = {{165,180,195},{165,180,195},{160,175,190},{170,185,200},{170,185,200},{180,195,210},{150,165,180},{150,165,180},{155,165,180},{170,185,200},{170,185,200},{170,185,200}};
+int frappucino_addons_prices[7] = {20,30,20,20,20,20,20};
+char *frappucino_addons[50] = {"Flavored Syrup","Espresso Shot","White Mocha","Heavy Cream","Cold Foam","Organic Soymilk","Breve"};
+
+char *teavanas[50] = {"Ice Shaken Black Tea with Ruby Grapefruit and Honey","Ice Shaken Hibiscus Tea with Pomegranate Pearls",};
+int teavana_prices[13][3] = {{140,155,170},{140,155,170},{155,170,185},{125,140,155},{125,140,155},{125,140,155},{140,155,170},{155,170,185},{155,170,185},{120,135,150},{125,140,155},{125,140,155},{125,140,155}};
+
+int all_orders[50][4];
+//all orders acts as a index/placeholder for all order information
+    //all_orders[n][m], n corresponds to a each order iteration (essentially each order made by the user, first order, second order etc.)
+    //all_orders[n][m], m corresponds to each order information
+        //all_orders[n][0], 0 correponds to order type either/menu choice (espresso, frappucino, tea)
+        //all_orders[n][1], 1 corresponds to size (tall, grande, venti)
+        //all_orders[n][2], 2 corresponds to quantity
+        //all_orders[n][3], 3 corresponds to the user order itself.
+
+
+
+
 //displays list of beverage options user can select.
 void display_menu() {
     printf("Menu:\n");
@@ -48,14 +75,15 @@ void menu_frapuccino() {
 void menu_customize() {
     printf("\nCustomize Your Beverage\n");
     printf("ADD\n");
-    printf("   - Flavored Syrup  +20\n");
-    printf("   - Espresso Shot   +30\n");
-    printf("   - White Mocha     +20\n");
-    printf("   - Heavy Cream     +20\n");
-    printf("   - Cold Foam       +20\n");
+    printf("(1.)   - Flavored Syrup  +20\n");
+    printf("(2.)   - Espresso Shot   +30\n");
+    printf("(3.)   - White Mocha     +20\n");
+    printf("(4.)   - Heavy Cream     +20\n");
+    printf("(5.)   - Cold Foam       +20\n");
     printf("Substitute\n");
-    printf("   - Organic Soymilk +20\n");
-    printf("   - Breve           +20\n");
+    printf("(6.)   - Organic Soymilk +20\n");
+    printf("(7.)   - Breve           +20\n");
+    printf("\nPlease enter the number of your addon of choice: \n");
 }
 
 //displays teavanatea options
@@ -79,6 +107,7 @@ void menu_teavanatea() {
     printf("(12.) Black Full-Leaf                                                 125        140         155\n");
     printf("(13.) Caffeine-free Herbal Tea Full-Leaf                              125        140         155\n");
     printf("\nPlease enter the number of your drink of choice: \n");
+
  
 }
     
@@ -115,37 +144,6 @@ int ask_for_quantity() {
     return quantity;
 }
 
-//collates information and summarizes order. Also returns final price of order
-int order(menu_choice,drink_size,user_order,order_quantity,extra){
-    int i,price;
-    //prices of espressos arranged respective to their menu order ex: espresso[0][1] corresponds to 145; or a grande[1] caffe americano[0]
-    int espresso_prices[8][3] = {{130,145,160},{140,155,170},{140,155,170},{165,180,195},{165,180,195},{165,180,195},{165,180,195},{120,130,0}};
-    
-    int frappucino_prices[12][3] = {{165,180,195},{165,180,}};
-    int frappucino_addons[7] = {20,30,20,20,20,20,20,20};
-
-    int teavana_prices[13][3] = {};
-    
-    if (menu_choice==1)
-    {
-        price = (espresso_prices[user_order-1][drink_size-1])*order_quantity; 
-    }
-    else if (menu_choice==2) //extra in this instance, are the optional add ons the user can add to their order if they choose the frappacino option
-    {
-        price = ((frappucino_prices[user_order-1][drink_size-1])*order_quantity)+extra;     
-    }
-    else if (menu_choice==3)//for menu options 1 & 3, extra is set to zero and does not influence its final price(see void(main) as to how these values are set/equated). 
-    {
-        price = ((teavana_prices[user_order-1][drink_size-1])*order_quantity)+extra;     
-    }
-    
-    return price;
-
-}
-
-void receipt(menu_choice,drink_size,order_quantity,user_order){
-}
-
 int main(void) {
     char done[10];
     int menu_choice;
@@ -156,50 +154,52 @@ int main(void) {
     int user_order;
     int extra = 0;
     int order_iteration = 0;
-    int all_orders[50][4];
     int i;
-    //all orders acts as a index/placeholder for all order information
-    //all_orders[n][m], n corresponds to a each order iteration (essentially each order made by the user, first order, second order etc.)
-    //all_orders[n][m], m corresponds to each order information
-        //all_orders[n][0], 0 correponds to order type either, (0 = menu choice; espresso, frappucino, tea)
-        //all_orders[n][1], 1 corresponds to size (tall, grande, venti)
-        //all_orders[n][2], 2 corresponds to quantity
-        //all_orders[n][3], 3 corresponds to the user order itself.
+    int total = 0;
 
     do {
         display_menu();
         printf("What type of drink would you like?");
         scanf("%d", &menu_choice);
-        order();
-
-        ask_for_order();
-        scanf("%d", &menu_choice);
         all_orders[order_iteration][0] = menu_choice;
+        //sets column 0 equal to menu choice
 
         drink_size = select_size();
-        all_orders[order_iteration][1] = select_size;
+        all_orders[order_iteration][1] = drink_size;
+        //sets row n, column 1 to drink size
 
         order_quantity = ask_for_quantity();
         all_orders[order_iteration][2] = order_quantity;
+        //sets row n, column 2 to order quantity
         
         if (menu_choice == 1) { // Espresso
             menu_espresso();
             scanf("%i",&user_order);    
             hot_or_iced = ask_hot_or_iced();
-            order(menu_choice,drink_size,user_order,order_quantity,extra);
+            all_orders[order_iteration][3] = user_order;
         }
         else if (menu_choice == 2) { // Frappuccino
             menu_frapuccino();
             scanf("%i",&user_order);
             add_on_choice = ask_for_addons();
-            order(menu_choice,drink_size,user_order,order_quantity,add_on_choice);   
+            all_orders[order_iteration][3] = user_order;
+            if (add_on_choice == 1)
+            {
+                order_iteration++;
+                all_orders[order_iteration][0] = 4;
+                menu_customize();
+                scanf("%i",&user_order);
+                all_orders[order_iteration][3] = user_order;
+                order_quantity = ask_for_quantity();
+                all_orders[order_iteration][2] = order_quantity;
+            }
+              
         }
         else if(menu_choice == 3){
             menu_teavanatea();
             scanf("%i",&user_order);
-            order(menu_choice,drink_size,user_order,order_quantity,extra);
+            all_orders[order_iteration][3] = user_order;
         }
-        all_orders[order_iteration][3] = user_order;
 
         printf("\nWould you like to order another drink? (Type 'yes' to add and 'done' to finish): ");
         scanf("%s", done);
@@ -209,11 +209,34 @@ int main(void) {
     while (strcmp(done, "done") != 0); {
         printf("\nProceeding to payment...\n");
         //print receipt
+        printf("Item            Price       Quantity        Subtotal");
+
         for (i = 0; i < order_iteration+1; i++)
         {
-            /* code */
+            //printf("\n%i",i);
+            if (all_orders[i][0] == 1)
+            {
+                total = total + ((espresso_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);
+                printf("\n%s     %i         %i           %i",espressos[all_orders[i][3]-1],espresso_prices[all_orders[i][3]][all_orders[i][1]-1],all_orders[i][2],(espresso_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);  
+            }
+            else if (all_orders[i][0] == 2)
+            {
+                total = total + ((frappucino_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);
+                printf("\n%s     %i         %i           %i",frappucino[all_orders[i][3]-1],frappucino_prices[all_orders[i][3]][all_orders[i][1]-1],all_orders[i][2],(frappucino_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);  
+            }
+            else if (all_orders[i][0] == 3)
+            {
+                total = total + ((teavana_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);
+                printf("\n%s     %i         %i           %i",teavanas[all_orders[i][3]-1],teavana_prices[all_orders[i][3]][all_orders[i][1]-1],all_orders[i][2],(teavana_prices[all_orders[i][3]][all_orders[i][1]-1])*all_orders[i][2]);  
+            }
+            else if (all_orders[i][0] == 4)
+            {
+                total = total + ((frappucino_addons_prices[all_orders[i][3]])*all_orders[i][2]);
+                printf("\n%s     %i         %i           %i",frappucino_addons[all_orders[i][3]-1],frappucino_addons_prices[all_orders[i][3]],all_orders[i][2],(frappucino_addons_prices[all_orders[i][3]])*all_orders[i][2]);  
+            }
+            
         }
-        
+        printf("\nTotal Php                              %i",total);
     }
 
     return 0;
